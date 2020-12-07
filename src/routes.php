6b187d79a27d38ec2fdbd6dd402c20c75e19e0de
Edwin F sturt <mailme@edwinsturt.in>
2020-12-08 01:10:43 +0530

    routes

--- src/routes.php
+++ src/routes.php
@@ -4,6 +4,6 @@ Route::group([
     'middleware' => ['guest']
 ], function () {
 
-    Route::get('address/test', 'AddressController@createAddress');
+    Route::get('address/test', '\sturtuk\phputils\Controllers\AddressController@createAddress');
 
 });

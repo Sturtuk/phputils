00b7504b1f4799dfb7a74db1d59955d08191d36c
Edwin F sturt <mailme@edwinsturt.in>
2020-12-09 00:13:54 +0530

    upgraded packages

--- src/routes.php
+++ src/routes.php
@@ -6,6 +6,8 @@ Route::group([
     'middleware' => ['guest']
 ], function () {
 
-    Route::get('address/test', '\sturtuk\phputils\Controllers\AddressController@createAddress');
+    Route::get('address/create', '\sturtuk\phputils\Controllers\AddressController@createAddress');
+    Route::get('address/find/{id}', '\sturtuk\phputils\Controllers\AddressController@find');
+    Route::get('address/searchByRadius', '\sturtuk\phputils\Controllers\AddressController@findWithinRadius');
 
 });
